a7eb0e52967f586fab9901d4c6f9ffd56e417af3
Jam Hsu <jam.hsu@lativ.com.tw>
2022-12-19 15:20:06 +0800

    Fix prefix sum

--- src/PrefixSum.java
+++ src/PrefixSum.java
@@ -1,6 +1,6 @@
 public class PrefixSum {
   void prefixSum(int[] nums) {
-    int[] prefix = new int[nums.length];
+    int[] prefix = new int[nums.length + 1];
     for (int i = 1; i < prefix.length; i++) {
       prefix[i] = prefix[i - 1] + nums[i - 1];
     }
