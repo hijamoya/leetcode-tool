be2127cee5f3e8f8f719ffd2e600a3bb70864629
Jam <jam.hsu@lativ.com.tw>
2024-01-07 16:10:39 +0800

    Update diff

--- src/DigitDp.java
+++ src/DigitDp.java
@@ -1,7 +1,13 @@
 public class DigitDp {
   int k;
+  Integer[][][][] memo;
 
-  int dfs(Integer[][][][] memo, String num, int r, int i, int even, int odd, boolean isLimit, boolean isNum) {
+  int main() {
+    // 注意isLimit最一開始是true
+    return dfs(num, r,0, 0, 0, true, false);
+  }
+
+  int dfs(String num, int r, int i, int even, int odd, boolean isLimit, boolean isNum) {
     if (i == num.length()) {
       return isNum && even == odd && r == 0 ? 1 : 0; // isNum 为 true 表示得到了一个合法数字
     }
@@ -11,12 +17,12 @@ public class DigitDp {
     int res = 0;
     if (!isNum) {
       // 可以跳过当前数位
-      res = dfs(memo, num, r, i + 1, even, odd, false, false);
+      res = dfs(num, r, i + 1, even, odd, false, false);
     }
     int up = isLimit ? num.charAt(i) - '0' : 9;
     for (int d = isNum ? 0 : 1; d <= up; ++d) {
       int newR = (r * 10 + d) % k;
-      res += dfs(memo, num, newR, i + 1, even + (d % 2 == 0 ? 1 : 0), odd + (d % 2 == 1 ? 1 : 0), isLimit && d == up, true);
+      res += dfs(num, newR, i + 1, even + (d % 2 == 0 ? 1 : 0), odd + (d % 2 == 1 ? 1 : 0), isLimit && d == up, true);
     }
     if (!isLimit && isNum) {
       // r的教訓
