323e4445a7024fe9dca3aca24e7a33690e611fe5
Jam <jam.hsu@lativ.com.tw>
2024-02-15 08:43:27 +0800

    Update kmp

--- src/Kmp.java
+++ src/Kmp.java
@@ -15,6 +15,7 @@ public class Kmp {
     return next;
   }
 
+  // 回傳第一個下標位置
   int strStr(String heystack, String needle) {
     if (needle.length() == 0) {
       return 0;
@@ -33,5 +34,28 @@ public class Kmp {
       }
     }
     return -1;
+  }
+
+  // 回傳總共有多少個模式串
+  int count(String heystack, String needle) {
+    int count = 0;
+    if (needle.length() == 0) {
+      return 0;
+    }
+    int[] next = getNext(needle);
+    int j = -1;
+    for (int i = 0; i < heystack.length(); i++) {
+      while (j >= 0 && heystack.charAt(i) != needle.charAt(j + 1)) {
+        j = next[j];
+      }
+      if (heystack.charAt(i) == needle.charAt(j + 1)) {
+        j++;
+      }
+      if (j == needle.length() - 1) {
+        j = next[j];
+        count++;
+      }
+    }
+    return count;
   }
 }
