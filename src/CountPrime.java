5cee614f9cd98bc06943c812a4c081853907f703
Jam <jam.hsu@lativ.com.tw>
2024-02-18 13:03:13 +0800

    Add count prime

---
+++ src/CountPrime.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class CountPrime {
+    // 埃氏篩
+    // 算出小於n的所有質數
+    int n = 99999;
+    boolean[] primes = new boolean[n + 1];
+
+    void count() {
+        Arrays.fill(primes, true);
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                continue;
+            }
+            for (int j = i * i; j <= n; j += i) {
+                primes[j] = false;
+            }
+        }
+    }
+
+    boolean isPrime(int k) {
+        return primes[k];
+    }
+}
