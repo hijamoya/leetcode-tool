a9cd4e310ec4af60d71ab4efa46851200839b169
Jam <jam.hsu@lativ.com.tw>
2024-03-20 08:49:57 +0800

    Add mod version mypow

--- src/FastPow.java
+++ src/FastPow.java
@@ -11,4 +11,16 @@ public class FastPow {
     long k = myPow(x, n / 2);
     return n % 2 == 0 ? (k * k) % mod : (k * k * x) % mod;
   }
+
+  long myPow2(long x, long n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (n == 1) {
+      return x;
+    }
+    long k = myPow(x, n / 2);
+    long k2 = (k * k) % mod;
+    return n % 2 == 0 ? k2 : (k2 * x) % mod;
+  }
 }
