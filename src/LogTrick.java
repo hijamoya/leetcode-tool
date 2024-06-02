e5eb5d5f48164fe5e3a531a530a0a1c818bbaf75
Jam <jam.hsu@lativ.com.tw>
2024-06-02 19:14:01 +0800

    Update

--- src/LogTrick.java
+++ src/LogTrick.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 力扣上有很多这样的题，需要用到一个结论：
@@ -9,13 +11,14 @@ import java.util.List;
  */
 // https://leetcode.cn/problems/smallest-subarrays-with-maximum-bitwise-or/solutions/1830911/by-endlesscheng-zai1/
 public class LogTrick {
-    void tricks(int[] nums) {
+    void logTricks(int[] nums) {
         int n = nums.length;
         List<int[]> list = new ArrayList<>(); // 按位(and, or, lcm, gcd)的值 + 对应子数组的右端点的最小值
         for (int i = n - 1; i >= 0; i--) {
             list.add(new int[]{nums[i], i});
             int index = 0;
             for (int[] op : list) {
+                // 這裡是And，可以改任意operation
                 op[0] &= nums[i];
                 if (list.get(index)[0] == op[0]) {
                     list.get(index)[1] = op[1]; // 合并相同值，下标取最小的
@@ -24,8 +27,25 @@ public class LogTrick {
                     list.set(index, op);
                 }
             }
+            // 把尾巴刪掉
             list.subList(index + 1, list.size()).clear();
             // TODO: 這邊就有目前所有的值，如果题目改成任意给定数值，可以在 list 中查找
+        }
+    }
+
+    // 用set的版本
+    void logTricks2(int[] nums) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int i = n - 1; i >= 0; i--) {
+            Set<Integer> newSet = new HashSet<>();
+            newSet.add(nums[i]);
+            for (int op : set) {
+                // 這裡是And，可以改任意operation
+                newSet.add(op & nums[i]);
+            }
+            // TODO: 值都在newSet裡面
+            set = newSet;
         }
     }
 }
